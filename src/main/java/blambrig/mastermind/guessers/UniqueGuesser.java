cd4c6faf6b74ed0f78ca113472d02135d5068cd4
lambriggerbrian <lambrigger.brian@gmail.com>
2020-08-12 16:41:58 -0700

    Unique Guesser fix

--- src/main/java/blambrig/mastermind/guessers/UniqueGuesser.java
+++ src/main/java/blambrig/mastermind/guessers/UniqueGuesser.java
@@ -11,11 +11,6 @@ public class UniqueGuesser extends Guesser {
 	}
 	
 	@Override
-	protected Guess getFirstGuess() {
-		return new Guess(colorManager.slice(table.numCols));
-	}
-	
-	@Override
 	protected Guess nextGuess() {
 		Guess guess = super.nextGuess();
 		while (!guess.isUnique()) {
